c91c1282a96cf5b0f38e43a1be48913eefaac813
Mamba <84301026+lsoth@users.noreply.github.com>
2021-06-24 18:35:05 -0700

    cleaned up code for variables, created prompts and confirm messages that are to be used to build the function, created if statements to combine the arrays together

--- assets/script.js
+++ assets/script.js
@@ -20,34 +20,42 @@ generateBtn.addEventListener("click", writePassword);
 // THEN I am presented with a series of prompts for password criteria
 // Create variables to determine what criteria the password will contain
 
-var lowercase = ("abcdefghijklmnopqrstuvwxyz")
+var lowercase = ("abcdefghijklmnopqrstuvwxyz").split('')
 
-var lowercasePw = lowercase.split("")
+// var lowercasepw = lowercase.split('')
 
-console.log(lowercasePw)
+console.log(lowercase)
 
-var uppercase = ("ABCDEFGHIJKLMNOPQRSTUV")
+var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split('')
 
-var uppercasePw = uppercase.split("")
+// var uppercasePw = uppercase.split("")
 
-console.log(uppercasePw)
+console.log(uppercase)
 
-var numbers = ("0123456789")
+var numbers = ("0123456789").split('')
 
-var numbersPw = numbers.split("")
+// var numbersPw = numbers.split("")
 
-console.log(numbersPw)
+console.log(numbers)
 
-var symbols = ("~`! @#$%^&*()_-+={[}]|;'<,>.?/") 
+var symbols = ("~`! @#$%^&*()_-+={[}]|;'<,>.?/").split('') 
 
-var symbolsPw = symbols.split("")
+// var symbolsPw = symbols.split("")
 
-console.log(symbolsPw)
+console.log(symbols)
 
 
 //TODO: Set up series of prompts or alerts that confirms for numbers, upper or lower
 //case characters or special characters that the user decides, use conditional statements
-// to make sure at leaxt one statement is chosen
+// to make sure at least one statement is chosen
+
+var numChar = prompt ("How long would you like your password to be? (Between 8 and 128 characters)");
+
+var yesLower = confirm ("Would you like lowercase characters?");
+
+var yesUpper = confirm ("Would you like UPPERCASE characters?");
+
+var yesSpecial = confirm ("Would you like special characters?");
 
 
 //TODO: Take the confirms and combine into holder and randomize using using math.Floor, and math.Random
